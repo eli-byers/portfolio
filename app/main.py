e1019c53cb2ca86c26f125f519cc059ae575a6e2
Eli <elibyers.dev@gmail.com>
2019-09-21 16:10:09 -0700

    Update Resume
    Clearn up
    
    Fix

--- app/main.py
+++ app/main.py
@@ -1,18 +1,9 @@
-import os
 from flask import Flask, render_template, redirect, request, jsonify
-from flask_request_params import bind_request_params
-
 app = Flask(__name__, static_url_path='/static')
-app.before_request(bind_request_params)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/arcade')
-def arcade():
-    return render_template('arcade.html')
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
